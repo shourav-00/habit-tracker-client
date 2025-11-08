5da70b64f49484227399917f88ddc9768b7a83ea
shourav-00 <shouravchowdhury400@gmail.com>
2025-11-08 23:42:56 +0600

    Initialize React app for Habit Tracker project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+    </>
+  );
+}
+
+export default App;
